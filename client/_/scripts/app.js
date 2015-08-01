a57d9dbc2345ab87dedf227cadd6e209d71b6d19
Quinton Aiken <quintonlaiken@gmail.com>
2015-08-01 16:48:44 -0700

    add client dependencies

--- client/_/scripts/app.js
+++ client/_/scripts/app.js
@@ -1,6 +1,13 @@
 var $ = require('jquery');
 var socket = io.connect(window.location.hostname);
 
+var angular = require('angular');
+var test = require('angular-socket-io');
+
+var alertify = require('alertify');
+
+// alertify.alert('hey');
+
 socket.on('status', function (data) {  
   $('#status').html(data.status);
 });
