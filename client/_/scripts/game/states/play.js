66e21ea2865a157a3e87fb487da30d84db518c79
Quinton Aiken <quintonlaiken@gmail.com>
2015-08-02 23:42:11 -0700

    refactor phaser demo play state

--- client/_/scripts/game/states/play.js
+++ client/_/scripts/game/states/play.js
@@ -5,22 +5,27 @@ var play = function(Game) {
 
   Game.States.Play.prototype = {
     create: function() {
-      //  A Tilemap object just holds the data needed to describe the map (i.e. the json exported from Tiled).
+
+      this.initWorld();
+
+      this.initEmitter();
+
+      this.initPlayer();
+
+      this.cursors = this.input.keyboard.createCursorKeys();
+    },
+    initWorld: function() {
       this.map = this.add.tilemap('level3');
 
-      // The name of the tileset as specified in the map data
-      // and the key of the Phaser.Cache image used for this tileset.
       this.map.addTilesetImage('CybernoidMap3BG_bank.png', 'tiles');
 
       this.layer = this.map.createLayer(0);
 
-      // Exclude collisions on these tiles
       this.map.setCollisionByExclusion([7, 32, 35, 36, 47]);
 
       this.layer.resizeWorld();
-
-      this.cursors = this.input.keyboard.createCursorKeys();
-
+    },
+    initEmitter: function() {
       this.emitter = this.add.emitter(0, 0, 200);
 
       this.emitter.makeParticles('chunk');
@@ -28,16 +33,12 @@ var play = function(Game) {
       this.emitter.maxRotation = 0;
       this.emitter.gravity = 150;
       this.emitter.bounce.setTo(0.5, 0.5);
-
+    },
+    initPlayer: function() {
       this.sprite = this.add.sprite(300, 90, 'phaser');
       this.sprite.anchor.set(0.5);
 
       this.physics.enable(this.sprite);
-
-      //  Because both our body and our tiles are so tiny,
-      //  and the body is moving pretty fast, we need to add
-      //  some tile padding to the body. WHat this does
-      this.sprite.body.tilePadding.set(32, 32);
 
       this.camera.follow(this.sprite);
     },
@@ -46,30 +47,38 @@ var play = function(Game) {
       this.emitter.y = this.sprite.y;
       this.emitter.start(true, 2000, null, 1);
     },
-    update: function() {
+    checkCollisions: function() {
       this.physics.arcade.collide(this.sprite, this.layer);
       this.physics.arcade.collide(this.emitter, this.layer);
-
-      this.sprite.body.velocity.x = 0;
-      this.sprite.body.velocity.y = 0;
+    },
+    movePlayer: function(player) {
+      player.body.velocity.x = 0;
+      player.body.velocity.y = 0;
 
       if (this.cursors.up.isDown) {
-        this.sprite.body.velocity.y = -200;
+        player.body.velocity.y = -200;
         this.particleBurst();
       } else if (this.cursors.down.isDown) {
-        this.sprite.body.velocity.y = 200;
+        player.body.velocity.y = 200;
         this.particleBurst();
       }
 
       if (this.cursors.left.isDown) {
-        this.sprite.body.velocity.x = -200;
-        this.sprite.scale.x = -1;
+        player.body.velocity.x = -200;
+        player.scale.x = -1;
         this.particleBurst();
       } else if (this.cursors.right.isDown) {
-        this.sprite.body.velocity.x = 200;
-        this.sprite.scale.x = 1;
+        player.body.velocity.x = 200;
+        player.scale.x = 1;
         this.particleBurst();
       }
+    },
+    update: function() {
+
+      this.checkCollisions();
+
+      this.movePlayer(this.sprite);
+
     }
   };
 };
--- client/_/scripts/game/states/preloader.js
+++ client/_/scripts/game/states/preloader.js
@@ -6,9 +6,14 @@ var preloader = function(Game) {
   Game.States.Preloader.prototype = {
     preload: function() {
       this.load.tilemap('level3', 'tilemaps/cybernoid.json', null, Phaser.Tilemap.TILED_JSON);
+      
       this.load.image('tiles', 'img/cybernoid.png', 16, 16);
       this.load.image('phaser', 'img/phaser-ship.png');
       this.load.image('chunk', 'img/chunk.png');
+      this.load.image('bullet', 'img/bullet.png');
+
+      this.load.spritesheet('kaboom', 'img/explode.png', 128, 128);
+
     },
     create: function() {
       this.state.start('Play');
