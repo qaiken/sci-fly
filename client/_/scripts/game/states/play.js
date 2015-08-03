99e92d87214ae6738523f50bb9da3176eb7babe9
Quinton Aiken <quintonlaiken@gmail.com>
2015-08-03 00:17:29 -0700

    add bullets

--- client/_/scripts/game/states/play.js
+++ client/_/scripts/game/states/play.js
@@ -6,13 +6,18 @@ var play = function(Game) {
   Game.States.Play.prototype = {
     create: function() {
 
+      this.timeLastBulletShot = 0;
+
       this.initWorld();
 
       this.initEmitter();
 
       this.initPlayer();
 
+      this.initBullets();
+
       this.cursors = this.input.keyboard.createCursorKeys();
+      this.fireButton = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     },
     initWorld: function() {
       this.map = this.add.tilemap('level3');
@@ -42,14 +47,27 @@ var play = function(Game) {
 
       this.camera.follow(this.sprite);
     },
+    initBullets: function() {
+      this.bullets = this.add.group();
+      this.bullets.enableBody = true;
+      this.bullets.physicsBodyType = Phaser.Physics.ARCADE;
+      this.bullets.createMultiple(30, 'bullet');
+      this.bullets.setAll('anchor.x', 0.5);
+      this.bullets.setAll('anchor.y', 1);
+      this.bullets.setAll('outOfBoundsKill', true);
+      this.bullets.setAll('checkWorldBounds', true);
+    },
     particleBurst: function() {
       this.emitter.x = this.sprite.x;
       this.emitter.y = this.sprite.y;
       this.emitter.start(true, 2000, null, 1);
     },
-    checkCollisions: function() {
+    checkWallCollisions: function() {
       this.physics.arcade.collide(this.sprite, this.layer);
       this.physics.arcade.collide(this.emitter, this.layer);
+      this.physics.arcade.collide(this.bullets, this.layer, function(bullet,layer) {
+        bullet.kill();
+      });
     },
     movePlayer: function(player) {
       player.body.velocity.x = 0;
@@ -73,11 +91,52 @@ var play = function(Game) {
         this.particleBurst();
       }
     },
+    fireBullet: function() {
+
+      var facing = this.sprite.body.facing;
+
+      if( facing === 1 ) {
+        this.sprite.orientation = 'left';
+      } else if ( facing === 2 ) {
+        this.sprite.orientation = 'right';
+      }
+
+
+      // Prevent too many bullets from firing
+      if (this.time.now <= this.bulletTime) {
+        return;
+      }
+      //  Grab the first bullet we can from the pool
+      this.bullet = this.bullets.getFirstExists(false);
+
+      if (!this.bullet) {
+        return;
+      }
+
+      if( this.sprite.orientation === 'left' ) {
+        // flip bullet sprite
+        this.bullet.scale.x = -1;
+        this.bullet.reset(this.sprite.x - 8, this.sprite.y);
+        this.bullet.body.velocity.x = -400;
+      } else if( this.sprite.orientation === 'right' ) {
+        this.bullet.reset(this.sprite.x + 8, this.sprite.y);
+        this.bullet.body.velocity.x = 400;
+      }
+
+      this.bulletTime = this.time.now + 200;
+    },
+    isFiring: function() {
+      if (this.fireButton.isDown) {
+        this.fireBullet();
+      }
+    },
     update: function() {
 
-      this.checkCollisions();
+      this.checkWallCollisions();
 
       this.movePlayer(this.sprite);
+
+      this.isFiring();
 
     }
   };
